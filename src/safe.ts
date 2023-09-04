69e24b5ce784380153f4c40ea5cc9dc76e5e3ce0
Aniket Datta <datta.aniket7@gmail.com>
2023-09-05 01:29:36 +0530

    compiler&services

---
+++ src/safe.ts
@@ -0,0 +1,19 @@
+// interface Point {
+//   x: number;
+//   y: number;
+// }
+
+// function logPoint(p: Point) {
+//   console.log(`${p.x}, ${p.y}`);
+// }
+
+// // Logs "12, 26"
+
+// const point = { x: 12, y: 26 };
+// logPoint(point);
+
+const person = { name: "Aniket", job: "Messing things up" };
+
+person.name;
+
+// const person: any = { name: "Aniket", job: "Messing things up" };
